c683557cb94de3dfff6abb32332ee0091543713c
RAVYANSH SHARMA <ravyanshsharmasxs@gmail.com>
2025-08-06 16:23:06 +0530

    Add files via upload

---
+++ PROJECT_7_WHEATHER_APP.py
@@ -0,0 +1,35 @@
+
+import requests
+import json
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 140)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+while True:
+    city = input("Enter the city (or 'exit' to quit): ")
+    if city.lower() == "exit":
+        break
+    try:
+        url = f"https://api.weatherapi.com/v1/current.json?key=6f54cec5a187430581890551250608&q={city}"
+        r = requests.get(url)
+        data = json.loads(r.text)
+
+        temp = data["current"]["temp_c"]
+        wind = data["current"]["wind_kph"]
+        winddir = data["current"]["wind_dir"]
+        humidity = data["current"]["humidity"]
+
+        message = f"The weather in {city} is {temp} degrees Celsius, wind speed is {wind} kilometers per hour, wind direction is {winddir}, and humidity is {humidity} percent."
+        print(message)
+        speak(message)
+
+    except Exception as e:
+        print("Something went wrong:", e)
+        speak("Sorry, I could not get the weather information.")
